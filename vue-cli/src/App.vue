fd20b11a935a07daa2541e3eac51ceb921c239b5
miji <mijiunknown@gmail.com>
2022-10-10 01:29:33 +0900

    <Vue.js 시작하기> - Vue CLI, 싱글 파일 컴포넌트

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- app-header 컴포넌트에 props 바인딩 -->
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
+  </div>
+</template>
+
+<script>
+// 컴포넌트 불러오기
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  data: function () {
+    return {
+      str: "Header",
+    };
+  },
+  components: {
+    "app-header": AppHeader,
+  },
+  methods: {
+    renewStr: function () {
+      this.str = "Hi👋🏻";
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1>{{ propsdata }}</h1>
+    <button v-on:click="sendEvent">send</button>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ["propsdata"],
+  methods: {
+    sendEvent: function () {
+      // 이벤트를 상위(App.vue)로 올려보냄
+      this.$emit("renew");
+    },
+  },
+};
+</script>
+
+<style></style>
