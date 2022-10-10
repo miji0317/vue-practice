33dfa3a798a886847f345a1ef3931de6c02c4704
miji <mijiunknown@gmail.com>
2022-10-10 14:29:13 +0900

    <Vue.js 시작하기> - 사용자 입력폼 만들기

---
+++ vue-form/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <!-- submit.prevent - 이벤트 새로고침 막기 -->
+    <!-- event.preventDefault()와 동일 -->
+    <form v-on:submit.prevent="submitForm">
+      <div>
+        <label for="username">id: </label>
+        <input id="username" type="text" v-model="username" />
+      </div>
+      <div>
+        <label for="password">pw: </label>
+        <input id="password" type="text" v-model="password" />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      username: "hello",
+      password: "bye",
+    };
+  },
+  methods: {
+    submitForm: function () {
+      console.log(this.username, this.password);
+      var url = "https://jsonplaceholder.typicode.com/users";
+      var data = {
+        username: this.username,
+        password: this.password,
+      };
+      axios
+        .post(url, data)
+        .then(function (res) {
+          console.log(res);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
+
+<style></style>
