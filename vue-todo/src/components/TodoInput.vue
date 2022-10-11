c5c949d24972dc70b9498b7a5681e68aa4e7e921
joy <neederkmj@gmail.com>
2022-10-11 18:08:30 +0900

    <Vue.js 중급 강좌> - todolist

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      newTodoItem: ""
+    }
+  },
+  methods: {
+    addTodo: function() {
+      console.log(this.newTodoItem)
+      // 로컬스토리지에 저장 key, value 형태
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.newTodoItem = ''; // 초기화
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
